8688d7a2b8effa00330f12afa09cb923c9bb0472
Földesi Barnus <foldesibarnus@gmail.com>
2022-10-09 16:37:45 +0200

    cleaning up code

--- main.js
+++ main.js
@@ -6,26 +6,45 @@ const player = document.getElementById('player');
 const diamond = document.getElementById('diamond');
 const wall = document.getElementById('wall');
 
-  const world = [[['wall']['wall']['wall']['wall']['wall']['wall']['wall']['wall']['wall']['wall']['wall']]
-  [['floor']['floor']['floor']['floor']['floor']['floor']['floor']['floor']['floor']['floor']['floor']]
-  [['floor']['floor']['floor']['floor']['floor']['floor']['floor']['floor']['floor']['floor']['floor']]
-  [['floor']['floor']['floor']['floor']['floor']['floor']['floor']['wall']['floor']['floor']['floor']]
-  [['floor']['floor']['floor']['floor']['floor']['floor']['floor']['wall']['floor']['floor']['floor']]
-  [['floor']['floor']['floor']['floor']['floor']['floor']['floor']['wall']['floor']['floor']['floor']]
-  [['floor']['floor']['floor']['floor']['floor']['floor']['floor']['floor']['floor']['floor']['floor']]
-  [['floor']['floor']['floor']['floor']['floor']['floor']['floor']['floor']['floor']['floor']['floor']]
-  [['floor']['floor']['floor']['floor']['floor']['floor']['floor']['floor']['floor']['floor']['floor']]];
-  
-for (let n = 0; n < 10; n ++) {
-
-  for (let i = 0; i < 11; i++) {
-     ctx.drawImage(world [i][n] , i * 70, n * 70, 70, 70);
-}
-}
-ctx.drawImage(player, 350, 280, 70, 70);
-
 const size = 70;
 const position = {x: 5, y: 0};
+
+const world = [
+  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+  [1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0],
+  [1, 1, 1, 1, 1, 0, 0, 1, 0, 0, 0],
+  [1, 1, 1, 1, 1, 0, 0, 1, 1, 1, 1],
+  [1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0],
+  [1, 1, 1, 1, 0, 0, 0, 1, 0, 0, 0],
+  [1, 1, 1, 1, 0, 0, 0, 1, 0, 0, 0],
+  [1, 1, 1, 1, 0, 0, 0, 1, 0, 0, 0],
+  [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+  [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+  [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+  [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+  [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+];
+
+function update() {
+  for (let n = 0; n < 10; n ++) {
+  for (let i = 0; i < 10; i++) {
+    if (world[position.y+n][i] === 0){
+        ctx.drawImage(floor, i * 70, n * 70, 70, 70);
+    } else {
+        ctx.drawImage(wall, i * 70, n * 70, 70, 70);
+    }
+  }
+}
+ctx.drawImage(player, position.x * 70, 280, 70, 70);
+}
+
+update();
 
 function onKeyPress(event) {
     // Nyilak kezelése
@@ -36,26 +55,30 @@ function onKeyPress(event) {
         ctx.drawImage(floor, position.x * 70 + 70, 280, 70, 70);
         break;
       case 38:
-        position.y += 1;
-        
+      
+                  position.y -= 1;
+          update();
         break;
+          if (world[0 - position.y - 1][position.x] === 0) {
+
+        }
       case 39:
-        position.x += 1
-        ctx.drawImage(player, position.x * 70, 280, 70, 70);
-        ctx.drawImage(floor, position.x * 70 - 70, 280, 70, 70);
+          position.x += 1
+          ctx.drawImage(player, position.x * 70, 280, 70, 70);
+          ctx.drawImage(floor, position.x * 70 - 70, 280, 70, 70);
         break;
+        
       case 40:
-        position.y -= 1
+        position.y += 1
+        update();
         
         break;
+        
       case 32:
-        alert('space');
+          alert('x: ' + position.x + ', y: ' + position.y);
         break;
     }
   }
 
-  
-          //alert(`x: ${position.x}, y: ${position.y}`);
-          //alert('x: ' + position.x + ', y: ' + position.y);
 
   document.body.addEventListener('keydown', onKeyPress);
